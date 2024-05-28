ed087fc457896118d2c2f80b304425d4c33dc199
Samir Jr Abou-Serhal <sabouserhal@gmail.com>
2024-05-28 14:00:12 -0400

    Update Distance_warning_system.ino

--- Distance_warning_system/Distance_warning_system.ino
+++ Distance_warning_system/Distance_warning_system.ino
@@ -1,5 +1,7 @@
-#include "SR04.h"
-#include <LiquidCrystal.h>
+#include "SR04.h" //distance library
+#include <LiquidCrystal.h> //LCD library
+
+//pin definition
 #define TRIG_PIN 3
 #define ECHO_PIN 2
 int buzzer=13;
@@ -11,6 +13,7 @@ long distance;
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
 void setup() {
+   //initializing pins for devices
    Serial.begin(9600);
    pinMode(buzzer, OUTPUT);
    pinMode(red, OUTPUT);
@@ -20,15 +23,20 @@ void setup() {
 }
 
 void loop() {
-   distance=sr04.Distance();
-   Serial.print(distance);
+   distance=sr04.Distance(); //saving actual distance into a variable
+   Serial.print(distance); //allows monitoring in the serial monitor
    Serial.println("cm");
-   if (distance<10.0)
+   
+   if (distance<10.0) //first if statement if object is too close (within 10 cm)
    {
-      digitalWrite(red, HIGH);
-      digitalWrite(yellow, LOW);
+      digitalWrite(red, HIGH); //red LED is turned on
+      digitalWrite(yellow, LOW); //the other LEDs are off
       digitalWrite(green, LOW);
-      digitalWrite(buzzer, HIGH);
+      digitalWrite(buzzer, HIGH); //Buzzer starts making noise
+      delay(100); //delay is added to make alarm noise
+      digitalWrite(buzzer, LOW);
+      delay(100);
+      //LCD screen is readied and displays message
       lcd.clear();
       lcd.print("STEP AWAY NOW!");
       lcd.setCursor(0, 1);
@@ -37,27 +45,24 @@ void loop() {
       lcd.print(distance);
       lcd.setCursor(3, 1);
       lcd.print("cm");
-      delay(100);
-      digitalWrite(buzzer, LOW);
-      delay(100);
    }
-   if (distance>=10.0 && distance<=20.0)
+   if (distance>=10.0 && distance<=20.0) //similar logic as the previous condition
    {
       digitalWrite(red, LOW);
       digitalWrite(yellow, HIGH);
       digitalWrite(green, LOW);
       digitalWrite(buzzer, HIGH);
+      delay(450);
+      digitalWrite(buzzer, LOW);
+      delay(450);
       lcd.clear();
       lcd.print("You're too close");
       lcd.setCursor(0, 1);
       lcd.print(distance);
       lcd.setCursor(3, 1);
       lcd.print("cm");
-      delay(450);
-      digitalWrite(buzzer, LOW);
-      delay(450);
    }
-   if (distance>20.0)
+   if (distance>20.0) //similar conditions once again
    {
       digitalWrite(buzzer, LOW);
       digitalWrite(red, LOW);
